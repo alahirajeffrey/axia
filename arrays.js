c3eea91b8a060f1b872ae0455693804372db32bb
alahirajeffrey <alahirajeffrey@gmail.com>
2024-03-20 21:30:53 +0100

    added arras file

---
+++ arrays.js
@@ -0,0 +1,62 @@
+// Javascript array methods
+
+// const firstStudents = Array();
+// console.log(firstStudents);
+
+// const secondStudents = [];
+// console.log(secondStudents);
+
+const firstStudent = "Richard";
+const secondStudent = "Uko";
+const thirdStudent = "Samuel";
+
+// first way of creating arrays
+const cohortFive = [];
+// console.log(cohortFive);
+
+// second way of creating array
+// const secondCohortFive = Array();
+// console.log(secondCohortFive);
+
+// the push function adds to the end of the array
+cohortFive.push("Richard");
+// console.log(cohortFive);
+cohortFive.push("Uko");
+// console.log(cohortFive);
+cohortFive.push("Samuel");
+// console.log(cohortFive);
+cohortFive.push(15);
+// console.log(cohortFive);
+
+// the pop function removes from the end of the array
+// cohortFive.pop();
+// console.log(cohortFive);
+// cohortFive.pop();
+// console.log(cohortFive);
+// cohortFive.pop();
+// console.log(cohortFive);
+// cohortFive.pop();
+// console.log(cohortFive);
+
+// to know the length of an
+// console.log(cohortFive.length);
+
+// inserts an element to the start of the array
+cohortFive.unshift("Jeffrey");
+// console.log(cohortFive);
+
+// for (let i = 0; i < cohortFive.length; i++) {
+//   console.log(i);
+//   console.log(cohortFive[i]);
+// }
+
+// for (const index in cohortFive) {
+//   console.log(index);
+//   console.log(cohortFive[index]);
+// }
+
+cohortFive.forEach((cohortFive) => {
+  console.log(cohortFive);
+});
+
+// for ()
