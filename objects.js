e400a44df8797608458963b1b09d1dcc731dc039
alahirajeffrey <alahirajeffrey@gmail.com>
2024-03-20 21:31:20 +0100

    added objects file

---
+++ objects.js
@@ -0,0 +1,26 @@
+// const student = {};
+// console.log(student);
+
+// const student = Object();
+// console.log(student);
+
+// an array of objects
+const students = [
+  {
+    name: "uko",
+    gender: "female",
+    favoriteWord: "okay",
+  },
+  {
+    name: "Samuel",
+    gender: "male",
+    favoriteWord: "alright",
+  },
+  {
+    name: "Richard",
+    gender: "male",
+    favoriteWord: "aiit",
+  },
+];
+
+console.log(students[2]);
