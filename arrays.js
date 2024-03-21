7dd7704546520cf152b2e1a57450378f7dd48860
alahirajeffrey <alahirajeffrey@gmail.com>
2024-03-21 09:52:07 +0100

    updated code and improved comments

--- arrays.js
+++ arrays.js
@@ -1,62 +1,80 @@
-// Javascript array methods
+/**
+ * arrays are special variables which can hold more than one value.
+ * Lets say you wanted to store the names of the students in cohort five
+ */
 
-// const firstStudents = Array();
-// console.log(firstStudents);
-
-// const secondStudents = [];
-// console.log(secondStudents);
-
+// without arrays
 const firstStudent = "Richard";
 const secondStudent = "Uko";
 const thirdStudent = "Samuel";
 
-// first way of creating arrays
+/**
+ * while technically, it still works but imagine there were over a 500 students.
+ * you'd have to create 500 variables which is not very efficient.
+ * The answer is an array
+ */
+
+// first method of initializing arrays
+const firstMethod = Array();
+
+// second method of initializing arrays
+const secondMethod = [];
+
+// lets initialize an empty array to save the names of all the students
 const cohortFive = [];
-// console.log(cohortFive);
 
-// second way of creating array
-// const secondCohortFive = Array();
-// console.log(secondCohortFive);
-
-// the push function adds to the end of the array
+// the push function adds an element to the end of the array
 cohortFive.push("Richard");
-// console.log(cohortFive);
+console.log(cohortFive);
+
 cohortFive.push("Uko");
-// console.log(cohortFive);
+console.log(cohortFive);
+
 cohortFive.push("Samuel");
-// console.log(cohortFive);
-cohortFive.push(15);
-// console.log(cohortFive);
+console.log(cohortFive);
 
-// the pop function removes from the end of the array
-// cohortFive.pop();
-// console.log(cohortFive);
-// cohortFive.pop();
-// console.log(cohortFive);
-// cohortFive.pop();
-// console.log(cohortFive);
-// cohortFive.pop();
-// console.log(cohortFive);
+cohortFive.push("Faith");
+console.log(cohortFive);
 
-// to know the length of an
-// console.log(cohortFive.length);
+cohortFive.push("Jeremiah");
+console.log(cohortFive);
+
+// the pop function removes an element from the end of the array
+cohortFive.pop();
+console.log(cohortFive);
+
+cohortFive.pop();
+console.log(cohortFive);
+
+// get the length of array
+console.log(cohortFive.length);
 
 // inserts an element to the start of the array
 cohortFive.unshift("Jeffrey");
-// console.log(cohortFive);
+console.log(cohortFive);
 
-// for (let i = 0; i < cohortFive.length; i++) {
-//   console.log(i);
-//   console.log(cohortFive[i]);
-// }
+/**
+ * There are various methods to loop through an array. Some of them include
+ * 1. The for loop
+ * 2. The for in loop
+ * 3. The for each loop
+ */
 
-// for (const index in cohortFive) {
-//   console.log(index);
-//   console.log(cohortFive[index]);
-// }
+console.log("\nLooping using the for loop");
+for (let i = 0; i < cohortFive.length; i++) {
+  console.log(cohortFive[i]);
+}
 
+console.log("\nLooping using the for in loop");
+for (const index in cohortFive) {
+  console.log(cohortFive[index]);
+}
+
+console.log("\nLooping using the for each loop");
 cohortFive.forEach((cohortFive) => {
   console.log(cohortFive);
 });
 
-// for ()
+/**
+ *
+ */
